03d00adf6b314ae16daac39734965849f9072635
PKafka0320 <aiya0422@gmail.com>
2022-01-12 14:54:37 +0900

    #5

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,36 @@
-export const trending = (req, res) => res.send("Home Page Videos");
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "heelo1",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "heelo2",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "heelo3",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+  ];
+  res.render("home", { pageTitle: "Home", videos });
+};
 
-export const see = (req, res) => res.send("Watch Video");
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 
-export const edit = (req, res) => res.send("Edit Video");
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 
 export const search = (req, res) => res.send("Search Video");
 
